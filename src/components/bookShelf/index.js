671519448edc683ace672c916a65ccad11ed333a
Bruno Russi <brunoxd13@gmail.com>
2018-10-07 13:41:27 -0300

    feat: show and hide books bu shelf

--- src/components/bookShelf/index.js
+++ src/components/bookShelf/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Book from "../book";
 
-import { Card } from "antd";
+import { Card, Icon, Button } from "antd";
 
 import "./styles.css";
 
@@ -13,15 +13,38 @@ class BookShelf extends Component {
     onMoveBook: PropTypes.func.isRequired
   };
 
+  state = {
+    visible: true
+  };
+
+  showOrHideBooks = () => {
+    this.setState({ visible: !this.state.visible });
+  };
+
   render() {
     const { title, books, onMoveBook } = this.props;
 
     return (
-      <Card className="card-shelf" title={title}>
+      <Card
+        className="card-shelf"
+        title={title}
+        extra={
+          this.state.visible ? (
+            <Button onClick={this.showOrHideBooks}>
+              <Icon type="up" theme="outlined" />
+            </Button>
+          ) : (
+            <Button onClick={this.showOrHideBooks}>
+              <Icon type="down" theme="outlined" />
+            </Button>
+          )
+        }
+      >
         <div className="book-container">
-          {books.map(book => (
-            <Book key={book.id} book={book} onMoveBook={onMoveBook} />
-          ))}
+          {this.state.visible &&
+            books.map(book => (
+              <Book key={book.id} book={book} onMoveBook={onMoveBook} />
+            ))}
         </div>
       </Card>
     );
