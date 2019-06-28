448aceae85cfcb75762c2c2c482d921249619533
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-06-28 15:35:23 -0300

    fix: broking packges

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,9 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
+const { override, fixBabelImports } = require('customize-cra');
 
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], // change importing css to less
-    config,
-  );
-
-  config = rewireLess.withLoaderOptions({
-    javascriptEnabled: true,
-  })(config, env);
-  
-  return config;
-};
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+);
